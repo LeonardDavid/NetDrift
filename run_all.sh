787a9c8ffcadb2d60810232f1005bc6b970bdbef
Leonard David <leonard.bereholschi@tu-dortmund.de>
2025-02-04 09:27:12 +0100

    update

--- run_all.sh
+++ run_all.sh
@@ -242,9 +242,8 @@ then
         then
             if [ "$KERNEL_SIZE" = 3 ]
             then
-                # MODEL_PATH="models/model_fmnist9108.pt"
+                MODEL_PATH="models/model_fmnist9108.pt"
                 # MODEL_PATH="models/phase1/model_fmnist_rtfi_nomhl_nobh_L4_p01.pt"
-                MODEL_PATH="models/phase3/model_fmnist_rtfi_nomhl_bh_L2_p0.1.pt"
             elif [ "$KERNEL_SIZE" = 5 ]
             then
                 MODEL_PATH="models/model_fmnist5x5_9077.pt"
